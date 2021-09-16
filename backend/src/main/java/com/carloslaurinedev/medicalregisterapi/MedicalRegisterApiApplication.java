2ac06d58ea7e3af13377765707a233b0de80b2ed
Carlos Laurine <cvpinhofsa@gmail.com>
2021-09-15 21:09:40 -0300

    Initialized Backend with Java 11 (LTS), Model Version 2.4.4 and Maven Dependencies (MySQL Driver, Spring Web, Spring Data JPA, H2 Test Database, DevTools and Hibernate Bean Validation IO Library)

---
+++ backend/src/main/java/com/carloslaurinedev/medicalregisterapi/MedicalRegisterApiApplication.java
@@ -0,0 +1,13 @@
+package com.carloslaurinedev.medicalregisterapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedicalRegisterApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedicalRegisterApiApplication.class, args);
+	}
+
+}
